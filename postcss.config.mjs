d1a8cc6bf100a9a673b73b54f92e764db42b3450
Adrian Szell <adrian.szell@scademy.com>
2025-08-11 20:59:51 +0200

    fix

---
+++ postcss.config.mjs
@@ -0,0 +1,12 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss,
+    autoprefixer,
+  },
+};
+
+export default config;
